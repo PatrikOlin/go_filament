e1b090be14e511138aa9fe1cd514b6fedecdf21b
Patrik Olin <enmejl@gmail.com>
2020-05-04 20:30:08 +0200

    changed database to postgresql and added support for .env-file

--- models/setup.go
+++ models/setup.go
@@ -1,12 +1,23 @@
 package models
 
 import (
+	"os"
+	"fmt"
+
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/joho/godotenv"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
 )
 
 func SetupModels() *gorm.DB {
-	db, err := gorm.Open("sqlite3", "spools.db")
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	fmt.Println(os.ExpandEnv("host=${HOST} user=${USER} dbname=${DBNAME} sslmode=disable password=${PASSWORD}"))
+	db, err := gorm.Open("postgres", os.ExpandEnv("host=${HOST} user=${USER} dbname=${DBNAME} sslmode=disable password=${PASSWORD}"))
 
 	if err != nil {
 		panic("Failed to connect to database!")
@@ -16,3 +27,15 @@ func SetupModels() *gorm.DB {
 
 	return db
 }
+
+// func SetupModels() *gorm.DB {
+// 	db, err := gorm.Open("sqlite3", "spools.db")
+
+// 	if err != nil {
+// 		panic("Failed to connect to database!")
+// 	}
+
+// 	db.AutoMigrate(&Spool{})
+
+// 	return db
+// }
