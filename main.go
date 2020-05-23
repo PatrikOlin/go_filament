3d359409c0ada1296ebff676c5718ef0809eabc9
Patrik Olin <enmejl@gmail.com>
2020-05-23 14:15:48 +0200

    added file logging

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"io"
+	"time"
 	"github.com/gin-gonic/gin"
 
 	"github.com/PatrikOlin/go_filament/models"
@@ -13,10 +16,17 @@ func main() {
 
 	db := models.SetupModels()
 
+	t := time.Now()
+	f, _ := os.Create("spools_gin" + t.Format(time.RFC3339)  + ".log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
 	r.Use(func(c *gin.Context) {
 		c.Set("db", db)
 		c.Next()
 	})
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
 
 	r.GET("/spools", controllers.FindSpools)
 	r.POST("/spools", controllers.CreateSpool)
